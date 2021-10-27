6f37ce3386993017e2e597ce0c3fc0c566c134a1
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-27 17:14:50 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React, { useState, useEffect } from 'react';
 import './style.css';
 
 function App() {
-  let [dogImage, setDogImage] = useState(null);
+  let [photos, setphotos] = useState(null);
 
   // 3. Create out useEffect function
   useEffect(() => {
-    fetch('https://dog.ceo/api/breeds/image/random/3')
+    fetch('https://jsonplaceholder.typicode.com/photos')
       .then((response) => response.json())
-      // 4. Setting *dogImage* to the image url that we received from the response above
-      .then((data) => setDogImage(data.message));
+      // 4. Setting *photos* to the image url that we received from the response above
+      .then((data) => setphotos(data));
   }, []);
-
+  console.log(photos);
   return (
     <div className="App">
       {/* 5. Returning an img element for each url, again with the value of our src set to the image url */}
-      {dogImage &&
-        dogImage.map((dog) => (
-          <img width={'200px'} height={'200px'} src={dog}></img>
+      {photos &&
+        photos.map((item) => (
+          <a href={item.url}>
+            <img width={'200px'} height={'200px'} src={item.thumbnailUrl}></img>
+          </a>
         ))}
     </div>
   );
